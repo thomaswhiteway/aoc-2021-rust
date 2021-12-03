b52af634ec05911d41e8e0bc452cbca720e9c704
Thomas Whiteway <thomas.whiteway@gmail.com>
2021-12-03 19:04:16 +0000

    Day 3 Part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -29,38 +29,83 @@ fn get_bit_counts(values: &[String]) -> Box<[usize]> {
     counts.into_boxed_slice()
 }
 
-fn get_bit_value<F>(bit_counts: &[usize], bit_set: F) -> usize
-where
-    F: Fn(usize) -> bool,
-{
-    let value_str: String = bit_counts
+fn get_most_common_bits(total: usize, bit_counts: &[usize]) -> String {
+    bit_counts
         .iter()
-        .map(|c| if bit_set(*c) { "1" } else { "0" })
-        .collect();
-    usize::from_str_radix(&value_str, 2).unwrap()
+        .map(|c| if c * 2 >= total { '1' } else { '0' })
+        .collect()
 }
 
-fn get_gamma(total: usize, bit_counts: &[usize]) -> usize {
-    get_bit_value(bit_counts, |c| c * 2 > total)
+fn flip_bits(input: &str) -> String {
+    input
+        .chars()
+        .map(|c| if c == '0' { '1' } else { '0' })
+        .collect()
 }
 
-fn get_epsilon(total: usize, bit_counts: &[usize]) -> usize {
-    get_bit_value(bit_counts, |c| c * 2 < total)
+fn parse_base2(input: &str) -> usize {
+    usize::from_str_radix(input, 2).unwrap()
 }
 
 fn get_power_consumption(values: &[String]) -> usize {
     let bit_counts = get_bit_counts(values);
 
-    let gamma = get_gamma(values.len(), &bit_counts);
-    let epsilon = get_epsilon(values.len(), &bit_counts);
+    let most_common_bits = get_most_common_bits(values.len(), &bit_counts);
+    let least_common_bits = flip_bits(&most_common_bits);
+
+    let gamma = parse_base2(&most_common_bits);
+    let epsilon = parse_base2(&least_common_bits);
 
     gamma * epsilon
+}
+
+fn get_rating<F>(values: &[String], take_set: F) -> usize
+where
+    F: Fn(usize, usize) -> bool,
+{
+    let mut remaining: Vec<&str> = values.iter().map(String::as_str).collect();
+    let mut index = 0;
+
+    while remaining.len() > 1 {
+        let (set, unset): (Vec<_>, Vec<_>) = remaining
+            .iter()
+            .partition(|val| val.chars().nth(index) == Some('1'));
+
+        remaining = if take_set(set.len(), unset.len()) {
+            set
+        } else {
+            unset
+        };
+
+        index += 1;
+    }
+
+    parse_base2(remaining[0])
+}
+
+fn get_oxygen_rating(values: &[String]) -> usize {
+    get_rating(values, |set, unset| set >= unset)
+}
+
+fn get_co2_rating(values: &[String]) -> usize {
+    get_rating(values, |set, unset| set < unset)
+}
+
+fn get_life_support_rating(values: &[String]) -> usize {
+    let oxygen_generator_rating = get_oxygen_rating(values);
+    let co2_scrubber_rating = get_co2_rating(values);
+
+    oxygen_generator_rating * co2_scrubber_rating
 }
 
 fn main() {
     let opt = Opt::from_args();
 
     let values = read_values(&opt.input);
+
     let power_consumption = get_power_consumption(&values);
-    println!("{}", power_consumption);
+    println!("Power Consumption: {}", power_consumption);
+
+    let life_support_rating = get_life_support_rating(&values);
+    println!("Life Support Rating: {}", life_support_rating);
 }
