695f007b5355e6552579c9992cb3b6df1a6abab3
Thomas Whiteway <thomas.whiteway@gmail.com>
2021-12-07 17:39:36 +0000

    Day 7 Part 2

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,7 +1,7 @@
+use std::collections::HashMap;
 use std::fs;
 use std::path::{Path, PathBuf};
 use structopt::StructOpt;
-use std::collections::HashMap;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -9,7 +9,7 @@ struct Opt {
     input: PathBuf,
 }
 
-type Crabs = HashMap<usize, usize>;
+type Crabs = HashMap<isize, isize>;
 
 fn read_crabs<P: AsRef<Path>>(input: P) -> Crabs {
     let mut crabs = HashMap::new();
@@ -18,7 +18,7 @@ fn read_crabs<P: AsRef<Path>>(input: P) -> Crabs {
     let positions = data
         .trim_end()
         .split(',')
-        .map(|num| num.parse::<usize>().unwrap());
+        .map(|num| num.parse::<isize>().unwrap());
 
     for position in positions {
         (*crabs.entry(position).or_default()) += 1;
@@ -27,12 +27,12 @@ fn read_crabs<P: AsRef<Path>>(input: P) -> Crabs {
     crabs
 }
 
-fn find_min_fuel_to_align(crabs: &Crabs) -> usize {
-    let mut current_fuel: usize = crabs.iter().map(|(position, count)| position * count).sum();
-    let mut left_crabs: usize = crabs.get(&0).cloned().unwrap_or_default();
-    let mut right_crabs: usize = crabs.values().sum::<usize>() - left_crabs;
+fn find_min_linear_fuel_to_align(crabs: &Crabs) -> isize {
+    let mut current_fuel: isize = crabs.iter().map(|(position, count)| position * count).sum();
+    let mut left_crabs: isize = crabs.get(&0).cloned().unwrap_or_default();
+    let mut right_crabs: isize = crabs.values().sum::<isize>() - left_crabs;
     let mut position = 0;
-    
+
     while right_crabs > left_crabs {
         current_fuel -= right_crabs - left_crabs;
 
@@ -40,16 +40,40 @@ fn find_min_fuel_to_align(crabs: &Crabs) -> usize {
         let new_crabs = crabs.get(&position).cloned().unwrap_or_default();
         left_crabs += new_crabs;
         right_crabs -= new_crabs;
+    }
 
-    }   
-    
     current_fuel
+}
+
+fn find_min_quadratic_fuel_to_align(crabs: &Crabs) -> isize {
+    let min_pos = crabs.keys().min().cloned().unwrap();
+    let max_pos = crabs.keys().max().cloned().unwrap();
+
+    let fuel_to_move_all_crabs = |pos: isize| {
+        crabs
+            .iter()
+            .map(|(crab_pos, num_crabs)| num_crabs * fuel_to_move_one_crab(pos, *crab_pos))
+            .sum::<isize>()
+    };
+
+    fn fuel_to_move_one_crab(pos: isize, crab_pos: isize) -> isize {
+        let distance = (crab_pos - pos).abs();
+        (distance * (distance + 1)) / 2
+    }
+
+    (min_pos..=max_pos)
+        .map(fuel_to_move_all_crabs)
+        .min()
+        .unwrap()
 }
 
 fn main() {
     let opt = Opt::from_args();
 
     let crabs = read_crabs(&opt.input);
-    let min_fuel = find_min_fuel_to_align(&crabs);
+    let min_fuel = find_min_linear_fuel_to_align(&crabs);
+    println!("{}", min_fuel);
+
+    let min_fuel = find_min_quadratic_fuel_to_align(&crabs);
     println!("{}", min_fuel);
 }
