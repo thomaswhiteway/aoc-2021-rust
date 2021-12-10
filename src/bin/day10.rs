b9164d89ee94f8b25236f5e6d513d7dab985aad3
Thomas Whiteway <thomas.whiteway@gmail.com>
2021-12-10 09:04:25 +0000

    Day 10 Part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -17,43 +17,67 @@ fn read_program<P: AsRef<Path>>(input: P) -> Box<[String]> {
         .into_boxed_slice()
 }
 
-fn opener(close: char) -> char {
-    match close {
-        ')' => '(',
-        ']' => '[',
-        '}' => '{',
-        '>' => '<',
-        _ => panic!("Not a close bracket: {}", close),
+enum ValidateResult {
+    Invalid(char),
+    Incomplete(String)
+}
+
+impl ValidateResult {
+    fn invalid_char(&self) -> Option<char> {
+        match *self {
+            ValidateResult::Invalid(c) => Some(c),
+            _ => None
+        }
+    }
+
+    fn remaining_string(&self) -> Option<&str> {
+        match self {
+            ValidateResult::Incomplete(remaining) => Some(&remaining),
+            _ => None
+        }
+    }    
+}
+
+fn closer(open: char) -> char {
+    match open {
+        '(' => ')',
+        '[' => ']',
+        '{' => '}',
+        '<' => '>',
+        _ => panic!("Not a open bracket: {}", open),
     }
 }
 
-fn find_invalid_char(line: &str) -> Option<char> {
+fn validate_line(line: &str) -> ValidateResult {
     let mut stack = vec![];
 
     for c in line.chars() {
         match c {
-            '(' | '[' | '{' | '<' => stack.push(c),
+            '(' | '[' | '{' | '<' => stack.push(closer(c)),
             ')' | ']' | '}' | '>' => {
-                let open = stack.pop();
-                if open != Some(opener(c)) {
-                    return Some(c);
+                let expected = stack.pop();
+                if expected != Some(c) {
+                    return ValidateResult::Invalid(c);
                 }
             }
             _ => panic!("Unexpected character {}", c),
         }
     }
 
-    None
+    let remaining = stack.into_iter().rev().collect();
+    ValidateResult::Incomplete(remaining)
 }
 
-fn find_invalid_chars(program: &[String]) -> impl Iterator<Item = char> + '_ {
+fn validate_program(program: &[String]) -> Box<[ValidateResult]> {
     program
         .iter()
         .map(String::as_str)
-        .filter_map(find_invalid_char)
+        .map(validate_line)
+        .collect::<Vec<_>>()
+        .into_boxed_slice()
 }
 
-fn char_score(c: char) -> usize {
+fn invalid_char_score(c: char) -> usize {
     match c {
         ')' => 3,
         ']' => 57,
@@ -63,10 +87,30 @@ fn char_score(c: char) -> usize {
     }
 }
 
+fn remaining_char_score(c: char) -> usize {
+    match c {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
+        _ => panic!("Unexpected remaining char: {}", c),
+    }
+}
+
+fn remaining_score(remaining: &str) -> usize {
+    remaining.chars().rev().enumerate().map(|(index, c)| 5_usize.pow(index as u32) * remaining_char_score(c)).sum()
+}
+
 fn main() {
     let opt = Opt::from_args();
 
     let program = read_program(opt.input);
-    let score: usize = find_invalid_chars(&program).map(char_score).sum();
-    println!("{}", score);
+    let validate_results = validate_program(&program);
+    let invalid_score: usize = validate_results.iter().filter_map(ValidateResult::invalid_char).map(invalid_char_score).sum();
+    println!("{}", invalid_score);
+
+    let mut remaining_scores: Vec<usize> = validate_results.iter().filter_map(ValidateResult::remaining_string).map(remaining_score).collect();
+    remaining_scores.sort();
+    let middle_score = remaining_scores[remaining_scores.len()/2];
+    println!("{}", middle_score);
 }
